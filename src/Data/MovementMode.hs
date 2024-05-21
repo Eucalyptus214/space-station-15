0937648053fdf46a9bf6c9c27f142afa80a9a9dc
fl215 <andrew.lua80@gmail.com>
2024-05-21 06:17:36 +0300

    movement mode utility

--- src/Data/MovementMode.hs
+++ src/Data/MovementMode.hs
@@ -1,4 +1,13 @@
 module Data.MovementMode( MovementMode(..)
-                       ) where
+                        , getModeSpeed
+                        ) where
 
 data MovementMode = Running | Walking | Crouching | Proning
+
+getModeSpeed :: MovementMode -> Float
+getModeSpeed mode =
+  case mode of
+    Running     -> 2
+    Walking     -> 1
+    Crouching   -> 0.5
+    Proning     -> 0.25
